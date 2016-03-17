6e52b60c80a68b8c37094e9c7da4a308ca7b7293
Jezreel <jezreel.go@gmail.com>
2016-03-17 15:16:56 -0700

    Scaffold member tests

---
+++ test/models/member_test.rb
@@ -0,0 +1,16 @@
+require 'test-helper'
+
+class MemberTest < ActiveSupport::TestCase
+
+  test "Member is not saved if email not provided" do
+
+  end
+
+  test "Member is not saved if username to provided" do
+
+  end
+
+  test "Member is not saved if password not provided" do
+    
+  end
+end
