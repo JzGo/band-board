207f6b8ee22870849454a3de66b3911374a1f530
Jezreel <jezreel.go@gmail.com>
2016-03-17 16:48:21 -0700

    First failing member tests

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,16 +1,19 @@
-require 'test-helper'
+require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
 
   test "Member is not saved if email not provided" do
-
+    member1 = Member.new(membername: "Mel", password: "12344321")
+    assert_not(member1.valid?, "Member is valid without email")
   end
 
-  test "Member is not saved if username to provided" do
-
+  test "Member is not saved if membername to provided" do
+    member2 = Member.new(email: "member2@example.com", password: "12344321")
+    assert_not(member2.valid?, "Member is valid without membername")
   end
 
   test "Member is not saved if password not provided" do
-    
+    member3 = Member.new(membername: "Mel", email: "member3@example.com")
+    assert_not(member3.valid?, "Member is valid without password")
   end
 end
