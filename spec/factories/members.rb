a7c16b08eca39edf866ea5e98015373ad5d37a7c
Jezreel <jezreel.go@gmail.com>
2016-03-18 15:18:05 -0700

    Create a member factory

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :member do
-    
+    sequence(:memberid) { |n| "weedilyweedily#{n}" }
+    sequence(:email) { |n| "weedilyman{n}@example.com" }
+    sequence(:password) { |n| "passwords#{n}"}
   end
 
 end
