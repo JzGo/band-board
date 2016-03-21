2d56a69d76c22c2c5757cdabe63d40bb5166f214
Jezreel <jezreel.go@gmail.com>
2016-03-21 16:50:46 -0700

    Make sure tests start at root

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,16 +5,17 @@ RSpec.feature "home", type: :feature do
   # pending "add some scenarios (or delete) #{__FILE__}"
   describe "signin" do
     it "shows signin form" do
-      get "/"
-      assert_select ".navbar", :text => "home"
+      visit "/"
     end
   end
   describe "signup" do
     it "shows signup form" do
+      visit "/"
     end
   end
   describe "getstarted" do
     it "shows signup form" do
+      visit "/"
     end
   end
   it "if member not signed in, shows welcome"
