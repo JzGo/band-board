269e4c86adc29a015436b54a3cd891b3c7c61b20
Jezreel <jezreel.go@gmail.com>
2016-03-21 15:24:28 -0700

    Scaffold some home page tests

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "home", type: :feature do
+  # pending "add some scenarios (or delete) #{__FILE__}"
+  describe "signin" do
+    it "shows signin form" do
+      get "/"
+      assert_select ".navbar", :text => "home"
+    end
+  end
+  describe "signup" do
+    it "shows signup form" do
+    end
+  end
+  describe "getstarted" do
+    it "shows signup form" do
+    end
+  end
+  it "if member not signed in, shows welcome"
+end
