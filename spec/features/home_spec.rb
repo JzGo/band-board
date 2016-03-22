c9ff22dd2c44ef4f3513fd6cfbbe8c4cee1f1e19
Jezreel <jezreel.go@gmail.com>
2016-03-22 13:32:43 -0700

    Add signin and signup tests

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,19 +3,31 @@ require 'rails_helper'
 
 RSpec.feature "home", type: :feature do
   # pending "add some scenarios (or delete) #{__FILE__}"
+  describe "home" do
+    it "welcomes user" do
+      visit "/"
+      expect(page).to have_content('BandBoard')
+    end
+  end
   describe "signin" do
     it "shows signin form" do
       visit "/"
+      click_on('Sign In')
+      expect(page).to have_content('Sign In')
     end
   end
   describe "signup" do
     it "shows signup form" do
       visit "/"
+      click_on('Sign Up')
+      expect(page).to have_content('Sign Up')
     end
   end
   describe "getstarted" do
     it "shows signup form" do
       visit "/"
+      click_on('Get Started!')
+      expect(page).to have_content('Sign Up')
     end
   end
   it "if member not signed in, shows welcome"
